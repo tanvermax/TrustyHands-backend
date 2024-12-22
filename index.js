41d805ed4b0b9f5a29642f1c06c8d977dae95370
tanver909 <tanveermahidi@gmial.com>
2024-12-22 21:15:46 +0600

    user post api created

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 const  port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // service_job
 // ZSJxoR0PyYs4JDmS
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.toqnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 app.use(cors());
 app.use(express.json());
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    const userCollection = client.db('service').collection("user")
+    // Connect the client to the server	(optional starting in v4.7)
+   
+    app.post('/user', async (req, res)=>{
+      const newuser = req.body;
+      const result = await userCollection.insertOne(newuser);
+      res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req,res)=>{
     res.send('service is getting ready ')
 })
-
-
 
 app.listen( port, ()=>{
     console.log("service portal is runnning on ", port);
